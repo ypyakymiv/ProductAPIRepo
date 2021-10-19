931b2d54730b5018a5780a4d21e7f3674081a9db
Yuri Yakymiv <ypyakymiv@gmail.com>
2021-10-19 18:31:51 -0400

    initial commit

---
+++ ProductsAPI/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace ProductsAPI.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
